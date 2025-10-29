e99c46beb64b5d3148cbd4c49394aad38318825b
KhongMeow <KhongMeow@gmail.com>
2025-10-29 08:43:01 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const checkPermission = () => {
+    if (!('serviceWorker' in navigator)) {
+        throw new Error("No support for service worker!")
+    }
+    if (!('Notification' in window)) {
+        throw new Error("No support fro notification API");
+    }
+}
+const registerSW = async () => {
+    const registration = await navigator.serviceWorker.register('sw.js');
+    return registration;
+}
+const requestNotificationPermission = async () => {
+    const permission = await Notification.requestPermission();
+    if (permission !== 'granted') {
+    throw new Error("Notification permission not granted")
+    } else {
+        new Notification ("Hello world");
+    }
+}
+checkPermission()
+registerSW()
+requestNotificationPermission()
